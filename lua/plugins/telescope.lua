ea002ca9efba0b0dc4c7456e4a0b2428f49c6907
Aaron Mueninghoff <mueninghoffa@gmail.com>
2026-01-05 16:43:41 -0800

    reformatted (autoformatter)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,41 +1,43 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  config = function()
-    require('telescope').setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<CR>"] = function(prompt_bufnr)
-              local action_state = require("telescope.actions.state")
-              local entry = action_state.get_selected_entry()
-              require("telescope.actions").close(prompt_bufnr)
-              local target = entry.path or entry.filename
-              if target then
-                vim.cmd("drop " .. target)
-              end
-            end,
-          },
-        },
-        -- Landscape mode
-        layout_strategy = 'center',
-        layout_config = {
-            anchor = 'S',
-            height = 0.35,
-            width = 0.90,
-            preview_cutoff = 1,
-        }
-      },
-      pickers = {
-        diagnostics = {
-          initial_mode = "normal", -- Start in normal mode for errors
-        },
-      },
-    })
+	"nvim-telescope/telescope.nvim",
+	config = function()
+		require("telescope").setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						-- Only show errors from the active file, and
+						-- return to the window that opened telescope
+						["<CR>"] = function(prompt_bufnr)
+							local action_state = require("telescope.actions.state")
+							local entry = action_state.get_selected_entry()
+							require("telescope.actions").close(prompt_bufnr)
+							local target = entry.path or entry.filename
+							if target then
+								vim.cmd("drop " .. target)
+							end
+						end,
+					},
+				},
+				-- Landscape mode
+				layout_strategy = "center",
+				layout_config = {
+					anchor = "S",
+					height = 0.35,
+					width = 0.90,
+					preview_cutoff = 1,
+				},
+			},
+			pickers = {
+				diagnostics = {
+					initial_mode = "normal", -- Start in normal mode for errors
+				},
+			},
+		})
 
-    -- Keymap for local diagnostics
-    vim.keymap.set('n', '<leader>d', function()
-      require('telescope.builtin').diagnostics({ bufnr = 0 })
-    end, { desc = '[D]iagnostics list (Current Buffer)' })
-  end,
+		-- Keymap for local diagnostics
+		vim.keymap.set("n", "<leader>d", function()
+			require("telescope.builtin").diagnostics({ bufnr = 0 })
+		end, { desc = "[D]iagnostics list (Current Buffer)" })
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,17 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        -- The require happens INSIDE this function
-        local configs = require("nvim-treesitter.config")
-        configs.setup({
-            ensure_installed = { "python", "lua", "vim", "vimdoc", "javascript", "html" },
-            highlight = { enable = true },
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		-- Force compiler on windows
+		if vim.fn.has("win32") == 1 then
+			vim.env.CC = "gcc"
+		end
+
+		local configs = require("nvim-treesitter.config")
+
+		configs.setup({
+			ensure_installed = { "python", "lua", "vim", "vimdoc", "javascript", "html" },
+			highlight = { enable = true },
+		})
+	end,
 }
