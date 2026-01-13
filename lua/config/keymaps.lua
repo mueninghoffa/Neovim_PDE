b9775a4a241b6e555e24ece5cc66dab8cce8b9fa
Aaron Mueninghoff <mueninghoffa@gmail.com>
2026-01-13 00:29:03 -0800

    --append

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,3 +17,8 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- Move visual blocks (Drag code up/down with J/K)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move block down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move block up" })
+
+-- Moving around context
+vim.keymap.set("n", "[c", function()
+	require("treesitter-context").go_to_context(vim.v.count1)
+end, { silent = true })
