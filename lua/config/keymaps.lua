d3d43a48a0d8158de2508a5c49828c76847e571f
Aaron Mueninghoff <mueninghoffa@gmail.com>
2026-02-16 14:26:37 -0800

    Added Ctrl+Del keymap and line/block movement keymaps. lazy-lock.json update included

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,9 +2,12 @@
 -- EDITING & TERMINAL-STYLE KEYS
 -- =============================================================================
 
--- Ctrl+Backspace to delete whole words (IDE behavior)
+-- Ctrl+Backspace to delete whole words
 vim.keymap.set("i", "<C-BS>", "<C-w>", { desc = "Delete word backward" })
 vim.keymap.set("i", "<C-H>", "<C-w>", { desc = "Delete word backward (Terminal fix)" })
+
+-- Ctrl+Delete to delete whole words
+vim.keymap.set("i", "<C-Del>", "<C-o>dw", { desc = "Delete word forwards" })
 
 -- =============================================================================
 -- NAVIGATION (CURSORS & PAGING)
@@ -14,9 +17,18 @@ vim.keymap.set("i", "<C-H>", "<C-w>", { desc = "Delete word backward (Terminal f
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- Move visual blocks (Drag code up/down with J/K)
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move block down" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move block up" })
+-- Normal Mode: Move current line
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+
+-- Visual Mode: Move selected block
+-- gv=gv re-selects the block and fixes indentation
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Insert Mode: Move line while typing
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down" })
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up" })
 
 -- Moving around context
 vim.keymap.set("n", "[c", function()
